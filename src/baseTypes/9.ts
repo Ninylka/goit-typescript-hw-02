c3aa02acfcd816fe374ee1e800fccc67bd1a4520
Ninylka <shemetylka@gmail.com>
2023-12-27 11:05:56 +0100

    first

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,20 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+
+
+type PageIfo = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close',
+  details?: {
+    createAt: Date,
+    updateAt: Date,
+  }
+}
+
+const page1: PageIfo = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -11,13 +24,16 @@ const page1 = {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
   }
-}
+};
 
-const page2 = {
+
+
+const page2: PageIfo = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
 }
+
 
 export {};
--- src/generic/7.ts
+++ src/generic/7.ts
@@ -3,6 +3,23 @@
   Ви хочете створити об'єкт RoleDescription, який зіставлятиме кожну роль користувача з її описом.
 */
 
+// export enum UserRole {
+//   admin = 'admin',
+//   editor = 'editor',
+//   guest = 'guest',
+// }
+
+// // Замініть наступний код на версію за допомогою Record
+// const RoleDescription = {
+//   admin: 'Admin User',
+//   editor: 'Editor User',
+//   guest: 'Guest User',
+// };
+
+// export {};
+
+
+
 export enum UserRole {
   admin = 'admin',
   editor = 'editor',
@@ -10,10 +27,10 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: 'Admin User',
+  [UserRole.editor]: 'Editor User',
+  [UserRole.guest]: 'Guest User'
 };
 
 export {};
